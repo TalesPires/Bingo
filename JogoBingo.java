567b302f73117d7144e2802cab7aee32a9499301
TalesPires <145354273+TalesPires@users.noreply.github.com>
2023-10-29 20:08:53 -0300

    Update JogoBingo.java

--- JogoBingo.java
+++ JogoBingo.java
@@ -20,8 +20,9 @@ public class JogoBingo {
             System.out.println("3. Excluir Cartela");
             System.out.println("4. Reinicializar Bingo");
             System.out.println("5. Sortear Próximo Número");
-            System.out.println("6. Consultas");
-            System.out.println("7. Sair");
+            System.out.println("6. Sortear todos os números");
+            System.out.println("7. Consultas");
+            System.out.println("8. Sair");
             System.out.println("Escolha uma opção: \n");
             int opcao = scanner.nextInt();
             scanner.nextLine(); // Limpa a quebra de linha
@@ -95,7 +96,7 @@ public class JogoBingo {
                             for (String jogador : jogadoresComNumeroSorteado) {
                                 if (!vencedores.contains(jogador) && bingo.verificarVencedor(jogador)) {
                                     vencedores.add(jogador);
-                                    System.out.println("Vencedor: " + jogador);
+                                    System.out.println("Vencedor(es): " + vencedores);
                                 }    
                             }   
                         } 
@@ -120,38 +121,79 @@ public class JogoBingo {
                     
                     break;
                 case 6:
-                System.out.println("=== Consultas ===");
-                System.out.println("1. Nomes dos Jogadores e suas Cartelas (por ordem de inclusão)");
-                System.out.println("2. Números Sorteados (por ordem de sorteio)");
-                System.out.println("3. Números Sorteados (em ordem crescente)");
-                System.out.println("4. Quantidade de Números Sorteados por Jogador (em ordem decrescente)");
-                System.out.println("5. Números Não Sorteados por Jogador (em ordem decrescente)");
-                System.out.print("Escolha uma opção de consulta: \n");
-                int opcaoConsulta = scanner.nextInt();
-                scanner.nextLine(); // Limpa a quebra de linha
-
-                switch (opcaoConsulta) {
-                    case 1:
-                        bingo.consultarCartelas();
-                        break;
-                    case 2:
-                        bingo.consultarNumerosSorteados();
-                        break;
-                    case 3:
-                        bingo.consultarNumerosSorteadosOrdenados();
-                        break;
-                    case 4:
-                        bingo.consultarQuantidadeNumerosSorteados();
-                        break;
-                    case 5:
-                        bingo.consultarNumerosNaoSorteados();
-                        break;
-                    default:
-                        System.out.println("Opção de consulta inválida.");
-                        break;
+                    while(vencedores.isEmpty()) {
+                        // Sortear Próximo Número
+                        if (numerosSorteados.size() < 99) {
+                            int numeroSorteado;
+                            do {
+                                numeroSorteado = random.nextInt(99) + 1;
+                            } while (numerosSorteados.contains(numeroSorteado));
+                            numerosSorteados.add(numeroSorteado);
+                            
+                            List<String> jogadoresComNumeroSorteado = bingo.verificarNumerosSorteados(numeroSorteado);
+                            if (!jogadoresComNumeroSorteado.isEmpty()) {
+                                System.out.println("Número sorteado: " + numeroSorteado);
+                                System.out.println("Jogador(es) com este número: " + jogadoresComNumeroSorteado);   
+                                // Verificar se há vencedores
+                                for (String jogador : jogadoresComNumeroSorteado) {
+                                    if (!vencedores.contains(jogador) && bingo.verificarVencedor(jogador)) {
+                                        vencedores.add(jogador);
+                                    }    
+                                }   
+                            } 
+                            
+                            System.out.println("Número sorteado: " + numeroSorteado);
+                            
+                            if (numerosSorteados.size() == 99){
+                                System.out.println("Todos os números foram sorteados");
+                            }
+                        } 
+                    }
+                    if (!vencedores.isEmpty()) {
+                        System.out.println("Jogo encerrado! Vencedor(es): " + vencedores);
+                        System.out.print("Deseja reinicializar o jogo (S/N)? Você pode conferir os dados do jogo atual se não reinicializa-lo: ");
+                        String resposta = scanner.nextLine();
+                        if (resposta.equalsIgnoreCase("S")) {
+                            bingo.reinicializarBingo();
+                            numerosSorteados.clear();
+                            vencedores.clear();
+                            System.out.println("Bingo reinicializado.");
+                        } 
                     }
                     break;
                 case 7:
+                    System.out.println("=== Consultas ===");
+                    System.out.println("1. Nomes dos Jogadores e suas Cartelas (por ordem de inclusão)");
+                    System.out.println("2. Números Sorteados (por ordem de sorteio)");
+                    System.out.println("3. Números Sorteados (em ordem crescente)");
+                    System.out.println("4. Quantidade de Números Sorteados por Jogador (em ordem decrescente)");
+                    System.out.println("5. Números Não Sorteados por Jogador (em ordem decrescente)");
+                    System.out.print("Escolha uma opção de consulta: \n");
+                    int opcaoConsulta = scanner.nextInt();
+                    scanner.nextLine(); // Limpa a quebra de linha
+    
+                    switch (opcaoConsulta) {
+                        case 1:
+                            bingo.consultarCartelas();
+                            break;
+                        case 2:
+                            bingo.consultarNumerosSorteados();
+                            break;
+                        case 3:
+                            bingo.consultarNumerosSorteadosOrdenados();
+                            break;
+                        case 4:
+                            bingo.consultarQuantidadeNumerosSorteados();
+                            break;
+                        case 5:
+                            bingo.consultarNumerosNaoSorteados();
+                            break;
+                        default:
+                            System.out.println("Opção de consulta inválida.");
+                            break;
+                        }
+                        break;
+                case 8:
                     // Sair
                     scanner.close();
                     System.exit(0);
